3b492867dedc19af4e83dfb50ebe30d4aa3580ec
Abdurrahman Shofy Adianto <azophy@gmail.com>
2022-07-27 22:03:28 +0700

    Update nik column definition

--- database/migrations/2022_07_27_082818_user_add_column_nik.php
+++ database/migrations/2022_07_27_082818_user_add_column_nik.php
@@ -14,7 +14,7 @@ class UserAddColumnNik extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('nik', 20)->nullable();
+            $table->string('nik', 20)->unique()->nullable();
         });
     }
 
