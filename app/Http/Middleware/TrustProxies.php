549a50cecf17c9dde1b81e6e9c4e73166f902817
Abdurrahman Shofy Adianto <azophy@gmail.com>
2022-07-29 17:31:26 +0700

    Config HTTP x-forwarded settings for heroku

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -10,9 +10,11 @@ class TrustProxies extends Middleware
     /**
      * The trusted proxies for this application.
      *
+     * ref: for Heroku settings: https://devcenter.heroku.com/articles/getting-started-with-laravel#trusting-the-load-balancer
+     *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    protected $proxies = '*';
 
     /**
      * The headers that should be used to detect proxies.
