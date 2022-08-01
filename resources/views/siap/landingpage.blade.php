732e1bb5b8a8770508c2f62e5add0cb0642da133
Abdurrahman Shofy Adianto <azophy@gmail.com>
2022-08-01 12:19:01 +0700

    Menambahkan dokumentasi & penjelasan di landing page

--- resources/views/siap/landingpage.blade.php
+++ resources/views/siap/landingpage.blade.php
@@ -1,21 +1,30 @@
 @extends('siap.layout')
 
 @section('content')
+<header style="text-align:left">
+    <details open="">
+        <summary>Penjelasan terkait web ini</summary>
+        <p>
+            Ini adalah website simulasi dari fungsionalitas di web SIAP. <strong>Web ini hanyalah contoh</strong>, dan tidak dimaksudkan untuk meniru persis fungsi yang ada di SIAP, namun hanya sebagai gambaran bagaimana ketika nanti SIAP sudah melakukan integrasi dengan sistem SSO Jabar.
+        </p>
+        <p>
+            Beberapa asumsi yang digunakan dalam implementasi web ini:
+        </p>
+
+        <ol>
+            <li>SIAP nantinya juga akan menggunakan SSO Jabar sebagai sistem loginnya</li>
+            <li>SIAP akan menjadi "source of truth" terkait data kepegawaian Pemprov Jabar untuk aplikasi-aplikasi Pemprov Jabar</li>
+        </ol>
+    </details>
+</header>
 <aside style="width:40%">
-    @auth
+   @auth
     <h2>Selamat datang, {{ Auth::user()->name }}</h2>
     <p>
         <button onclick="window.location='{{ route('oauth.logout') }}'">
             Logout from all application in this session
         </button>
     </p>
-    <h3>Details:</h3>
-    <pre>
-{{ print_r(Auth::user()->toArray()) }}
-
-NIK dari keycloak: {{ session('nik') }}
-Status User: {{ isAdmin(Auth::user()) ? 'ADMIN' : 'BUKAN ADMIN' }}
-    </pre>
     @else
     <strong>Anda belum login. Silahkan login terlebih dahulu</strong>
     <p>
@@ -24,14 +33,36 @@ Status User: {{ isAdmin(Auth::user()) ? 'ADMIN' : 'BUKAN ADMIN' }}
         </button>
     </p>
     @endauth
+
+    <h3>Session Details</h3>
+    <details>
+        <summary>Laravel/PHP Session ID</summary>
+        <p>Ini adalah ID dari sesi bawaan dari PHP. Setiap client yang mengunjungi web ini akan memiliki Session ID unik, yang nantinya ID ini bisa digunakan untuk proses "backchannel logout" dari Keycloak. Info lebih lanjut bisa cek referensi berikut:</p>
+        <ul>
+            <li><a target="_blank" href="https://www.php.net/manual/en/features.session.security.management.php">Dokumentasi terkait manajemen Session di PHP</a></li>
+            <li><a target="_blank" href="https://stackoverflow.com/questions/56863876/how-to-logout-user-from-specific-session-in-laravel/56864262#56864262">Diskusi stackoverflow terkait logout session Laravel secara remote</a></li>
+        </ul>
+    </details>
+    <p>
+        {{ \Session::getId() }}
+    </p>
+    <details>
+        <summary>Keycloak Session ID</summary>
+        <p>Ini adalah ID dari sesi login yang digenerate oleh Keycloak. Guna ID ini agar ketika logout, website ini bisa mencocokkan sesi ID dari laravel yang perlu dihapuskan berdasarkan request logout dari server Keycloak. Info lebih lanjut bisa cek source code di <code>App\Http\Controllers\OAuthController.php</code> di metode <code>logoutWebhook()</code></p>
+    </details>
+    <p>
+        {{ getCurrentKeycloakSessionId() ?: '-' }}
+    </p>
 </aside>
 <aside style="width:40%">
-    <h3>Session Details</h3>
-    <p>
-        <label for="">Laravel Session Id: </label> {{ \Session::getId() }}
-    </p>
-    <p>
-        <label for="">Keycloak Session Id: </label> {{ getCurrentKeycloakSessionId() ?: '-' }}
-    </p>
+    <h3>Detail user:</h3>
+    @auth
+    <pre>
+{{ print_r(Auth::user()->toArray()) }}
+
+NIK dari keycloak: {{ session('nik') }}
+Status User: {{ isAdmin(Auth::user()) ? 'ADMIN' : 'BUKAN ADMIN' }}
+    </pre>
+    @endauth
 </aside>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,6 +30,11 @@ $style_color = $color_choices[ env('STYLE_COLOR', 'teal') ];
   --primary-inverse: #FFF;
 }
 
+article {
+    margin:0;
+    padding:1rem;
+}
+
     </style>
 </head>
 <body style="background:var(--primary)">
@@ -40,86 +45,124 @@ $style_color = $color_choices[ env('STYLE_COLOR', 'teal') ];
         <h3>Example Laravel-Keycloak SSO Integration</h3>
       </hgroup>
   </header>
-  <main style="display:flex; gap:1rem; padding:1rem">
+  <main>
+    <article>
+        <details open="">
+            <summary>Penjelasan mengenai web ini</summary>
+            <p>
+                Website ini adalah simulasi dari website-website Pemprov Jabar setelah melakukan integrasi dengan sistem SSO Jabar & web SIAP.
+            </p>
+            <p>
+                Beberapa asumsi yang digunakan dalam implementasi web ini:
+            </p>
 
-    <article style="width:40%">
-        @auth
-        <strong>Selamat datang, {{ Auth::user()->name }}</strong>
-        <p>
-            <button onclick="window.location='{{ route('oauth.logout') }}'">
-                Logout from all application in this session
-            </button>
-        </p>
-        @else
-        <strong>Anda belum login. Silahkan login terlebih dahulu</strong>
-        <p>
-            <button onclick="window.location='{{ route('oauth.login') }}'">
-                Login with Jabar SSO
-            </button>
-        </p>
-        @endauth
-
-        <p>
-            <label for="">Laravel Session Id: </label> {{ \Session::getId() }}
-        </p>
-        <p>
-            <label for="">Keycloak Session Id: </label> {{ getCurrentKeycloakSessionId() ?: '-' }}
-        </p>
+            <ol>
+                <li>SIAP akan menjadi "source of truth" terkait data kepegawaian Pemprov Jabar untuk aplikasi ini</li>
+                <li>Untuk data pegawai selain menggunakan data dari SIAP, di sini akan disimulasikan bahwa ada juga data pegawai yang sudah ada di aplikasi, dan data tsb dapat disandingkan dengan data dari SIAP Jabar.</li>
+            </ol>
+        </details>
     </article>
 
-    <article style="width:60%">
-        <h2>Detail terkait user saat ini:</h2>
+    <br>
 
-        @auth
-        <p><strong>NIK user yang didapat dari keycloak: {{ session('nik') }}</strong></p>
-        <h3>
-            Data user yang sesuai dari API SIAP
-        </h3>
-        <pre>
-{{ print_r(getCurrentUserProfileFromSIAP()) }}
-        </pre>
+    <section style="display:flex; gap:1rem;">
+        <article style="width:40%">
+            @auth
+            <strong>Selamat datang, {{ Auth::user()->name }}</strong>
+            <p>
+                <button onclick="window.location='{{ route('oauth.logout') }}'">
+                    Logout from all application in this session
+                </button>
+            </p>
+            @else
+            <strong>Anda belum login. Silahkan login terlebih dahulu</strong>
+            <p>
+                <button onclick="window.location='{{ route('oauth.login') }}'">
+                    Login with Jabar SSO
+                </button>
+            </p>
+            @endauth
 
-        <h3>Data user dari keycloak</h3>
+            <details>
+                <summary><strong>Laravel/PHP Session ID</strong></summary>
+                <p>Ini adalah ID dari sesi bawaan dari PHP. Setiap client yang mengunjungi web ini akan memiliki Session ID unik, yang nantinya ID ini bisa digunakan untuk proses "backchannel logout" dari Keycloak. Info lebih lanjut bisa cek referensi berikut:</p>
+                <ul>
+                    <li><a target="_blank" href="https://www.php.net/manual/en/features.session.security.management.php">Dokumentasi terkait manajemen Session di PHP</a></li>
+                    <li><a target="_blank" href="https://stackoverflow.com/questions/56863876/how-to-logout-user-from-specific-session-in-laravel/56864262#56864262">Diskusi stackoverflow terkait logout session Laravel secara remote</a></li>
+                </ul>
+            </details>
+            <p>
+                {{ \Session::getId() }}
+            </p>
+            <details>
+                <summary><strong>Keycloak Session ID</strong></summary>
+                <p>Ini adalah ID dari sesi login yang digenerate oleh Keycloak. Guna ID ini agar ketika logout, website ini bisa mencocokkan sesi ID dari laravel yang perlu dihapuskan berdasarkan request logout dari server Keycloak. Info lebih lanjut bisa cek source code di <code>App\Http\Controllers\OAuthController.php</code> di metode <code>logoutWebhook()</code></p>
+            </details>
+            <p>
+                {{ getCurrentKeycloakSessionId() ?: '-' }}
+            </p>
+        </article>
 
-        <p>
-            Selain username, password, dan NIK, Keycloak juga menyimpan beberapa informasi lain yang ikut dikirimkan oleh server SSO ketika login: nama depan, nama belakang, serta email. Namun karena beberapa data ini beririsan dengan data yang disimpan di SIAP, disarankan agar menggunakan data dari SIAP saja agar menjadi 1 sumber "source of truth".
-        </p>
-        <details>
-            <summary>
-                <strong>
-                    Tampilkan data user dari Keycloak
-                </strong>
-            </summary>
+        <article style="width:60%">
+            <h2>Detail terkait user saat ini:</h2>
 
+            @auth
+            <p><strong>NIK user yang didapat dari keycloak: {{ session('nik') }}</strong></p>
+            <h3>
+                Data user yang sesuai dari API SIAP
+            </h3>
             <pre>
-    {{ print_r(session('KEYCLOAK_USER_DATA')) }}
+        {{ print_r(getCurrentUserProfileFromSIAP()) }}
             </pre>
-        </details>
 
-        <h3>Data user yang sesuai dari database app ini</h3>
+            <h3>Data user dari keycloak</h3>
 
-        <p>
-            Walaupun data profil bisa dikirimkan oleh data SIAP, tetap dimungkinkan untuk menyimpan data profil terkait user di dalam aplikasi/website pemerintahan, khususnya jika data yang dibutuhkan tidak tersedia di SIAP. Salah satu manfaat lain dari penyimpanan data di database aplikasi/website ini adalah untuk membedakan hak akses/permission dari suatu user hanya di aplikasi/website tersebut saja. Contoh user kepala BKD di SIAP bisa jadi memiliki hak akses admin, namun user tersebut di aplikasi mungkin saja memiliki role yang berbeda tergantung kebijakan pengelola aplikasi/website ybs.
-        </p>
-        <details>
-            <summary>
-                <strong>
-                    Tampilkan data user yang sesuai dari database app ini
-                </strong>
-            </summary>
+            <details open="">
+                <summary>Penjelasan:</summary>
+                <p>
+                    Selain username, password, dan NIK, Keycloak juga menyimpan beberapa informasi lain yang ikut dikirimkan oleh server SSO ketika login: nama depan, nama belakang, serta email. Namun karena beberapa data ini beririsan dengan data yang disimpan di SIAP, disarankan agar menggunakan data dari SIAP saja agar menjadi 1 sumber "source of truth".
+                </p>
+            </details>
+            <details>
+                <summary>
+                    <strong>
+                        Tampilkan data user dari Keycloak
+                    </strong>
+                </summary>
 
-            <pre>
-            @php
-            $localUserData = \App\Models\User::where('nik', session('nik'))->first();
-            $localUserData = $localUserData ? $localUserData->toArray() : null;
-            echo print_r($localUserData) ;
-            @endphp
-            </pre>
-        </details>
-        @else
-        <p>Not logged in</p>
-        @endif
-    </article>
+                <pre>
+        {{ print_r(session('KEYCLOAK_USER_DATA')) }}
+                </pre>
+            </details>
+
+            <h3>Data user yang sesuai dari database app ini</h3>
+
+            <details open="">
+                <summary>Penjelasan:</summary>
+                <p>
+                    Walaupun data profil bisa dikirimkan oleh data SIAP, tetap dimungkinkan untuk menyimpan data profil terkait user di dalam aplikasi/website pemerintahan, khususnya jika data yang dibutuhkan tidak tersedia di SIAP. Salah satu manfaat lain dari penyimpanan data di database aplikasi/website ini adalah untuk membedakan hak akses/permission dari suatu user hanya di aplikasi/website tersebut saja. Contoh user kepala BKD di SIAP bisa jadi memiliki hak akses admin, namun user tersebut di aplikasi mungkin saja memiliki role yang berbeda tergantung kebijakan pengelola aplikasi/website ybs.
+                </p>
+            </details>
+            <details>
+                <summary>
+                    <strong>
+                        Tampilkan data user yang sesuai dari database app ini
+                    </strong>
+                </summary>
+
+                <pre>
+                @php
+                $localUserData = \App\Models\User::where('nik', session('nik'))->first();
+                $localUserData = $localUserData ? $localUserData->toArray() : null;
+                echo print_r($localUserData) ;
+                @endphp
+                </pre>
+            </details>
+            @else
+            <p>Not logged in</p>
+            @endif
+        </article>
+    </section>
 
   </main>
 </body>
